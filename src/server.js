c19c9e7b7f1264cd29b6ca99788765fa5dfd1c72
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-28 17:44:38 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/status", (req, res) => {
+	console.log("it is alive!!!");
+
+	res.sendStatus(200);
+});
+
+server.listen(4000, () => console.log("Listening on port 4000..."));
