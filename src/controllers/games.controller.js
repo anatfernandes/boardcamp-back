c0250bcd45a213002ae6e5986b792f2074a0fd6f
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-29 16:51:04 -0300

    feat: get games on route GET /games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -53,4 +53,31 @@ async function createGame(req, res) {
 	res.sendStatus(STATUS_CODE.CREATED);
 }
 
-export { createGame };
+async function getGames(req, res) {
+	let games;
+
+	try {
+		games = await connection.query("SELECT * FROM games;");
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+	}
+
+	let categories;
+
+	try {
+		categories = await connection.query("SELECT * FROM categories;");
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+	}
+
+	const fullGames = games.rows.map((game) => ({
+		...game,
+		categoryName: categories.rows[game.categoryId - 1].name,
+	}));
+
+	res.status(STATUS_CODE.OK).send(fullGames);
+}
+
+export { createGame, getGames };
--- src/routers/games.router.js
+++ src/routers/games.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createGame } from "../controllers/games.controller.js";
+import { createGame, getGames } from "../controllers/games.controller.js";
 
 const router = express.Router();
 
 router.post("/games", createGame);
+router.get("/games", getGames);
 
 export default router;
