0f7747a392bca28db7bf444101424f7969e90829
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-30 13:36:14 -0300

    feat: get customers on route GET /customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -17,4 +17,26 @@ async function createCustomer(req, res) {
 	res.sendStatus(STATUS_CODE.CREATED);
 }
 
-export { createCustomer };
+async function getCustomers(req, res) {
+	const { cpf = "" } = req.query;
+
+	let customers;
+
+	try {
+		customers = await connection.query(
+			"SELECT * FROM customers WHERE cpf LIKE $1",
+			[`${cpf}%`]
+		);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+	}
+
+	customers.rows.forEach((customer) => {
+		customer.birthday = new Date(customer.birthday).toISOString().slice(0, 10);
+	});
+
+	res.status(STATUS_CODE.OK).send(customers.rows);
+}
+
+export { createCustomer, getCustomers };
--- src/routers/customers.router.js
+++ src/routers/customers.router.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createCustomer } from "../controllers/customers.controller.js";
+import {
+	createCustomer,
+	getCustomers,
+} from "../controllers/customers.controller.js";
 import { validateCustomerData } from "../middlewares/schemas.middleware.js";
 
 const router = express.Router();
 
 router.post("/customers", validateCustomerData, createCustomer);
+router.get("/customers", getCustomers);
 
 export default router;
