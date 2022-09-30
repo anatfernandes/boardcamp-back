3659a48af796d0a150d7b8d9b46d6e03398e5f44
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-30 11:27:33 -0300

    refactor: validation to create game

--- src/middlewares/schemas.middleware.js
+++ src/middlewares/schemas.middleware.js
@@ -1,6 +1,7 @@
 import connection from "../database/db.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
 import { categorieSchema } from "../schemas/categorie.schema.js";
+import { gameSchema } from "../schemas/game.schema.js";
 
 async function validateNameCategorie(req, res, next) {
 	const { name } = req.body;
@@ -30,4 +31,53 @@ async function validateNameCategorie(req, res, next) {
 	next();
 }
 
-export { validateNameCategorie };
+async function validateGameData(req, res, next) {
+	const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+	const isValidData = gameSchema.validate({
+		name,
+		image,
+		stockTotal,
+		categoryId,
+		pricePerDay,
+	});
+
+	if (isValidData.error) {
+		const errors = isValidData.error.details.map((error) => error.message);
+		return res.status(STATUS_CODE.BAD_REQUEST).send({ message: errors });
+	}
+
+	try {
+		const hasCategorie = await connection.query(
+			"SELECT * FROM categories WHERE id = $1;",
+			[categoryId]
+		);
+
+		if (hasCategorie.rows.length === 0)
+			return res
+				.status(STATUS_CODE.BAD_REQUEST)
+				.send({ message: "Id de categoria inválido." });
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+	}
+
+	try {
+		const hasName = await connection.query(
+			"SELECT * FROM games WHERE name ILIKE $1;",
+			[`${name}`]
+		);
+
+		if (hasName.rows.length !== 0)
+			return res
+				.status(STATUS_CODE.CONFLICT)
+				.send({ message: "Esse jogo já existe." });
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+	}
+
+	next();
+}
+
+export { validateNameCategorie, validateGameData };
--- src/routers/games.router.js
+++ src/routers/games.router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createGame, getGames } from "../controllers/games.controller.js";
+import { validateGameData } from "../middlewares/schemas.middleware.js";
 
 const router = express.Router();
 
-router.post("/games", createGame);
+router.post("/games", validateGameData, createGame);
 router.get("/games", getGames);
 
 export default router;
