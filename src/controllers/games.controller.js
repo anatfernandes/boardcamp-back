cdbb8e3195b6f8bb5f4dbc3bf109cee5f954f7f0
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-30 11:35:38 -0300

    refactor: method to merge games data and categories data

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -22,27 +22,15 @@ async function getGames(req, res) {
 	let games;
 
 	try {
-		games = await connection.query("SELECT * FROM games;");
+		games = (await connection
+			.query(`SELECT games.*, categories.name AS categoryName FROM games JOIN categories
+				ON games."categoryId" = categories.id;`)).rows;
 	} catch (error) {
 		console.log(error);
 		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
 	}
 
-	let categories;
-
-	try {
-		categories = await connection.query("SELECT * FROM categories;");
-	} catch (error) {
-		console.log(error);
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
-
-	const fullGames = games.rows.map((game) => ({
-		...game,
-		categoryName: categories.rows[game.categoryId - 1].name,
-	}));
-
-	res.status(STATUS_CODE.OK).send(fullGames);
+	res.status(STATUS_CODE.OK).send(games);
 }
 
 export { createGame, getGames };
