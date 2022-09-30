305cc5122b0740635189de8b73885d2851eb6ce4
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-30 11:02:09 -0300

    refactor: validation to create categorie

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -4,20 +4,6 @@ import { STATUS_CODE } from "../enums/statusCode.js";
 async function createCategorie(req, res) {
 	const { name } = req.body;
 
-	if (!name) return res.sendStatus(STATUS_CODE.BAD_REQUEST);
-
-	try {
-		const hasName = await connection.query(
-			"SELECT name FROM categories WHERE name = $1;",
-			[name]
-		);
-
-		if (hasName.rows.length !== 0) return res.sendStatus(STATUS_CODE.CONFLICT);
-	} catch (error) {
-		console.log(error);
-		return res.sendStatus(STATUS_CODE.SERVER_ERROR);
-	}
-
 	try {
 		await connection.query("INSERT INTO categories (name) VALUES ($1);", [
 			name,
--- src/routers/categories.router.js
+++ src/routers/categories.router.js
@@ -3,10 +3,11 @@ import {
 	createCategorie,
 	getCategorie,
 } from "../controllers/categories.controller.js";
+import { validateNameCategorie } from "../middlewares/schemas.middleware.js";
 
 const router = express.Router();
 
-router.post("/categories", createCategorie);
+router.post("/categories", validateNameCategorie, createCategorie);
 router.get("/categories", getCategorie);
 
 export default router;
